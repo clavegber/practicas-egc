71f065e9a39e93ec42d75163f476cc1eac45af9c
claulolo <claulolovb@gmail.com>
2017-01-10 16:50:25 +0100

    creada una nueva rama de desarrollo y añadida la funcionalidad de resta de dos enteros

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/6a/80885f4a4cd70016140c9a5b524b9fe5
@@ -0,0 +1,11 @@
+package main;
+
+public class Operaciones {
+	public static int Suma(int a,int b){
+		int result;
+		result = a+b;
+		System.out.println("Resultado de la suma" + a +"+" + b + ":" + result );
+		return result;
+	}
+	
+}
--- PracticasGit/src/main/Operaciones.java
+++ PracticasGit/src/main/Operaciones.java
@@ -4,8 +4,12 @@ public class Operaciones {
 	public static int Suma(int a,int b){
 		int result;
 		result = a+b;
-		System.out.println("Resultado de la suma" + a +"+" + b + ":" + result );
+		System.out.println("Resultado de la suma " + a +"+" + b + ":" + result );
 		return result;
 	}
-	
+	public static int Resta(int a,int b){
+		int result = a-b;
+		System.out.println("Resultado de la resta " + a + "-" + b + ":" + result);
+		return result;
+	}
 }
